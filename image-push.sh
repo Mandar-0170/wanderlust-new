ee4324519107a9010bc9814229fd0c50d0c7972c
Mandar <mandarpandit51@gmail.com>
2026-03-27 16:14:11 +0530

    image push fixed

--- image-push.sh
+++ image-push.sh
@@ -17,6 +17,7 @@ sudo systemctl enable docker
 echo "Adding user to docker group..."
 sudo usermod -aG docker $USER
 
+
 gcloud projects add-iam-policy-binding ferrous-linker-489911-f0 \
   --member="user:radnampandit21@gmail.com" \
   --role="roles/artifactregistry.writer"
@@ -24,19 +25,23 @@ gcloud projects add-iam-policy-binding ferrous-linker-489911-f0 \
 echo "Authenticating Docker with GCP..."
 gcloud auth configure-docker $REGION-docker.pkg.dev -q
 
+gcloud artifacts repositories add-iam-policy-binding registry-rd-dev-docker \
+  --location=us-central1 \
+  --member="serviceAccount:sa-rd-dev-bastion@ferrous-linker-489911-f0.iam.gserviceaccount.com" \
+  --role="roles/artifactregistry.writer"
 
 echo "Pushing Prometheus..."
-docker pull quay.io/prometheus/prometheus:v3.10.0
-docker tag quay.io/prometheus/prometheus:v3.10.0 \
+sudo docker pull quay.io/prometheus/prometheus:v3.10.0
+sudo docker tag quay.io/prometheus/prometheus:v3.10.0 \
 $REGION-docker.pkg.dev/$PROJECT_ID/$REPO/prometheus:v3.10.0
-docker push \
+sudo docker push \
 $REGION-docker.pkg.dev/$PROJECT_ID/$REPO/prometheus:v3.10.0
 
 echo "Pushing Grafana..."
-docker pull grafana/grafana:12.4.0
-docker tag grafana/grafana:12.4.0 \
+sudo docker pull grafana/grafana:12.4.0
+sudo docker tag grafana/grafana:12.4.0 \
 $REGION-docker.pkg.dev/$PROJECT_ID/$REPO/grafana:12.4.0
-docker push \
+sudo docker push \
 $REGION-docker.pkg.dev/$PROJECT_ID/$REPO/grafana:12.4.0
 
 echo "Done pushing images"
